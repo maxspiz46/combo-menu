53d1ff81d4e137fdae1f2eb5eafa2a5ccbe68a66
maxspiz46 <max-spitzer@dpsnc.net>
2026-03-23 19:36:39 +0000

    Clarifies remaining TODOs for 3.1.1, 3.2.1, and 3.2.3

--- main.py
+++ main.py
@@ -1,70 +1,59 @@
 order = []
 total_cost = 0.0
-print ("select sandhich")
-sandwich = input("chicken,tuna,veggie, no sandwich: ")
-if sandwich == "chicken": 
-    print("$13")
-    order.append(sandwich)
-    total_cost += 13
-elif sandwich == "tuna":
-    print("$15")
-    total_cost += 15
-    order.append(sandwich)
-elif sandwich == "veggie":
-    print("$100")
-    total_cost += 100
-    order.append(sandwich)
-elif sandwich == "no sandwich":
-    print ("select drink")
-drink = input("soda, lemonade, water, no drink: ")
-if drink == "soda":
-    soda = input("coke, sprite, Dr. Pepper: ")
-    if soda == "coke":
-       e = print ("$5")
-       total_cost += 5
-    if soda == "sprite":
-        r = print ("$4.50")
-        total_cost += 4.50
-    if soda == "Dr. Pepper":
-        t = print ("$5")
-        total_cost += 5
-if drink == "lemonade":
-    u = print ("$6")
-    total_cost += 6
-elif drink == "no drink":
-   m = input("Would you like desert?")
-   if m == "yes":
-      d = input ("cookie, cake, or brownie?")
-      if d == "cookie":
-          print ("$2.50")
-          total_cost += 2.5
-      if d == "cake":
-          print ("$3.75")
-          total_cost += 3.75
-      if d == "brownie":
-          print ("$3")
-          total_cost += 3
-# g = (0.0)
-# if sandwich == "chicken":
-#     g = (g + 13)
-# if sandwich == "tuna":
-#     g = (g + 15)
-# if sandwich == "veggie":
-#     g = (g + 100)
-# if drink == "lemonade":
-#     g = (g + 6)
-# if drink == "water":
-#     g = (g+0)
-# if soda == "coke":
-#     g = (g + 5)
-# if soda == "sprite":
-#     g = (g + 4.50)
-# if soda == "Dr. Pepper":
-#     g = (g + 5)
-# if d == "cookie":
-#     g = (g+2.50)
-# if d == "cake":
-#     g = (g+3.75)
-# if d == "brownie":
-#     g = (g+3)
-# print (g)
+order_requested = True
+while order_requested:
+    print ("select sandhich")
+
+    # Order the Sandwich
+    sandwich = input("chicken,tuna,veggie, no sandwich: ")
+    if sandwich == "chicken": 
+        print("$13")
+        order.append(sandwich)
+        total_cost += 13
+    elif sandwich == "tuna":
+        print("$15")
+        total_cost += 15
+        order.append(sandwich)
+    elif sandwich == "veggie":
+        print("$100")
+        total_cost += 100
+        order.append(sandwich)
+    elif sandwich == "no sandwich":
+        print ("select drink")
+
+    # Order the Drink
+    drink = input("soda, lemonade, water, no drink: ")
+    if drink == "soda":
+        soda = input("coke, sprite, Dr. Pepper: ")
+        if soda == "coke":
+            e = print ("$5")
+            total_cost += 5
+        if soda == "sprite":
+            r = print ("$4.50")
+            total_cost += 4.50
+        if soda == "Dr. Pepper":
+            t = print ("$5")
+            total_cost += 5
+    if drink == "lemonade":
+        u = print ("$6")
+        total_cost += 6
+    elif drink == "no drink":
+        pass # TODO: This seems wrong?!
+
+    # Order the dessert
+    m = input("Would you like desert?")
+    if m == "yes":
+        d = input ("cookie, cake, or brownie?")
+        if d == "cookie":
+            print ("$2.50")
+            total_cost += 2.5
+        if d == "cake":
+            print ("$3.75")
+            total_cost += 3.75
+        if d == "brownie":
+            print ("$3")
+            total_cost += 3
+
+    # TODO: What about sauces? Need to accept count from user to multiple sauce price by
+    # TODO: What about the combo special? Check for eligibility and discount total cost if necessary
+    
